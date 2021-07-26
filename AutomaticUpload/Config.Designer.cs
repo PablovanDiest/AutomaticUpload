dfa77a0685422e86aa2d00d88addc2f58911d499
Pablo van Diest <pvandiest@uniendomercados.com>
2021-07-26 18:35:46 -0300

    ...

---
+++ AutomaticUpload/Config.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace AutomaticUpload
+{
+    partial class Config
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtProjectPath = new System.Windows.Forms.TextBox();
+            this.lblProjectPath = new System.Windows.Forms.Label();
+            this.btnProjectPath = new System.Windows.Forms.Button();
+            this.btnAddConfig = new System.Windows.Forms.Button();
+            this.lstProject = new System.Windows.Forms.ListBox();
+            this.lstActivity = new System.Windows.Forms.ListBox();
+            this.lblRemotePath = new System.Windows.Forms.Label();
+            this.txtRemotePath = new System.Windows.Forms.TextBox();
+            this.btnRemotePath = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtProjectPath
+            // 
+            this.txtProjectPath.Location = new System.Drawing.Point(128, 25);
+            this.txtProjectPath.Name = "txtProjectPath";
+            this.txtProjectPath.Size = new System.Drawing.Size(569, 20);
+            this.txtProjectPath.TabIndex = 0;
+            // 
+            // lblProjectPath
+            // 
+            this.lblProjectPath.Location = new System.Drawing.Point(12, 25);
+            this.lblProjectPath.Name = "lblProjectPath";
+            this.lblProjectPath.Size = new System.Drawing.Size(110, 20);
+            this.lblProjectPath.TabIndex = 1;
+            this.lblProjectPath.Text = "Carpeta del proyecto";
+            // 
+            // btnProjectPath
+            // 
+            this.btnProjectPath.Location = new System.Drawing.Point(703, 19);
+            this.btnProjectPath.Name = "btnProjectPath";
+            this.btnProjectPath.Size = new System.Drawing.Size(32, 30);
+            this.btnProjectPath.TabIndex = 2;
+            this.btnProjectPath.Text = "...";
+            this.btnProjectPath.UseVisualStyleBackColor = true;
+            this.btnProjectPath.Click += new System.EventHandler(this.btnProjectPath_Click);
+            // 
+            // btnAddConfig
+            // 
+            this.btnAddConfig.Location = new System.Drawing.Point(741, 51);
+            this.btnAddConfig.Name = "btnAddConfig";
+            this.btnAddConfig.Size = new System.Drawing.Size(32, 30);
+            this.btnAddConfig.TabIndex = 3;
+            this.btnAddConfig.Text = ">>";
+            this.btnAddConfig.UseVisualStyleBackColor = true;
+            this.btnAddConfig.Click += new System.EventHandler(this.btnAddConfig_Click);
+            // 
+            // lstProject
+            // 
+            this.lstProject.FormattingEnabled = true;
+            this.lstProject.Location = new System.Drawing.Point(15, 103);
+            this.lstProject.Name = "lstProject";
+            this.lstProject.Size = new System.Drawing.Size(392, 303);
+            this.lstProject.TabIndex = 4;
+            // 
+            // lstActivity
+            // 
+            this.lstActivity.FormattingEnabled = true;
+            this.lstActivity.Location = new System.Drawing.Point(422, 103);
+            this.lstActivity.Name = "lstActivity";
+            this.lstActivity.Size = new System.Drawing.Size(497, 303);
+            this.lstActivity.TabIndex = 5;
+            this.lstActivity.SelectedIndexChanged += new System.EventHandler(this.lstActivity_SelectedIndexChanged);
+            // 
+            // lblRemotePath
+            // 
+            this.lblRemotePath.Location = new System.Drawing.Point(12, 57);
+            this.lblRemotePath.Name = "lblRemotePath";
+            this.lblRemotePath.Size = new System.Drawing.Size(110, 20);
+            this.lblRemotePath.TabIndex = 7;
+            this.lblRemotePath.Text = "Carpeta remota";
+            // 
+            // txtRemotePath
+            // 
+            this.txtRemotePath.Location = new System.Drawing.Point(128, 57);
+            this.txtRemotePath.Name = "txtRemotePath";
+            this.txtRemotePath.ReadOnly = true;
+            this.txtRemotePath.Size = new System.Drawing.Size(569, 20);
+            this.txtRemotePath.TabIndex = 6;
+            // 
+            // btnRemotePath
+            // 
+            this.btnRemotePath.Location = new System.Drawing.Point(703, 51);
+            this.btnRemotePath.Name = "btnRemotePath";
+            this.btnRemotePath.Size = new System.Drawing.Size(32, 30);
+            this.btnRemotePath.TabIndex = 8;
+            this.btnRemotePath.Text = "...";
+            this.btnRemotePath.UseVisualStyleBackColor = true;
+            this.btnRemotePath.Click += new System.EventHandler(this.btnRemotePath_Click);
+            // 
+            // Config
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(931, 414);
+            this.Controls.Add(this.btnRemotePath);
+            this.Controls.Add(this.lblRemotePath);
+            this.Controls.Add(this.txtRemotePath);
+            this.Controls.Add(this.lstActivity);
+            this.Controls.Add(this.lstProject);
+            this.Controls.Add(this.btnAddConfig);
+            this.Controls.Add(this.btnProjectPath);
+            this.Controls.Add(this.lblProjectPath);
+            this.Controls.Add(this.txtProjectPath);
+            this.Name = "Config";
+            this.Text = "Automatic Upload";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtProjectPath;
+        private System.Windows.Forms.Label lblProjectPath;
+        private System.Windows.Forms.Button btnProjectPath;
+        private System.Windows.Forms.Button btnAddConfig;
+        private System.Windows.Forms.ListBox lstProject;
+        private System.Windows.Forms.ListBox lstActivity;
+        private System.Windows.Forms.Label lblRemotePath;
+        private System.Windows.Forms.TextBox txtRemotePath;
+        private System.Windows.Forms.Button btnRemotePath;
+    }
+}
+
---
+++ AutomaticUpload/Config.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AutomaticUpload
+{
+    public partial class Config : Form
+    {
+        public Config()
+        {
+            InitializeComponent();
+        }
+
+        #region Private Events
+        private void btnProjectPath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            folderDialog.Description = this.lblProjectPath.Text;
+            folderDialog.ShowNewFolderButton = false;
+            folderDialog.SelectedPath = this.txtProjectPath.Text;
+            DialogResult result = folderDialog.ShowDialog(this);
+
+            if (result == DialogResult.OK)
+            {
+                this.txtProjectPath.Text = folderDialog.SelectedPath;
+            }
+        }
+
+        private void btnRemotePath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            folderDialog.Description = this.lblRemotePath.Text;
+            folderDialog.ShowNewFolderButton = false;
+            folderDialog.SelectedPath = this.txtRemotePath.Text;
+            DialogResult result = folderDialog.ShowDialog(this);
+
+            if (result == DialogResult.OK)
+            {
+                this.txtRemotePath.Text = folderDialog.SelectedPath;
+            }
+        }
+
+        private void btnAddConfig_Click(object sender, EventArgs e)
+        {
+            this.checkForChanges(this.txtProjectPath.Text);
+        }
+
+        private void OnChanged(object source, FileSystemEventArgs e)
+        {
+            this.lstActivity.Items.Add("File: " + e.FullPath + " " + e.ChangeType);
+        }
+
+        private void OnRenamed(object source, RenamedEventArgs e)
+        {
+            this.lstActivity.Items.Add(String.Format("File: {0} renamed to {1}", e.OldFullPath, e.FullPath));
+        }
+        #endregion
+
+        #region Private Methods
+        private void checkForChanges(String path)
+        {
+            this.CreateFileWatcher(path);
+        }
+
+        //public void AppendListViewcalls(string input)
+        //{
+        //    this.Dispatcher.Invoke(DispatcherPriority.Normal, new Action(delegate()
+        //    {
+        //        this.lstResults.Items.Add(input);
+        //    }));
+        //}  
+
+        private void CreateFileWatcher(string path)
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = path;
+            /* Watch for changes in LastAccess and LastWrite times, and 
+               the renaming of files or directories. */
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+               | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            // Only watch text files.
+            //watcher.Filter = "*.txt";
+
+            // Add event handlers.
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+            watcher.Deleted += new FileSystemEventHandler(OnChanged);
+            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+
+            // Begin watching.
+            watcher.EnableRaisingEvents = true;
+        }
+        #endregion
+
+        private void lstActivity_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
